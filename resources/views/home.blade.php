182c9e2aac639ed43c7ed16fa14ce4dc5ac12620
Shabnam Developer <P.shabnamghaemi@gmail.com>
2022-05-07 15:31:00 +0430

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="text-center">
+        <h1>Welcome to Laravel ToDo App</h1>
+        <hr/>
+
+        @include('partials.flash_notification')
+    </div>
+@endsection
